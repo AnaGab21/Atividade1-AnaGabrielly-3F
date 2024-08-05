9ca8342dc154386de40678d6a55fde8c811a50a6
AnaGab21 <ana.gabriellyde.lima@escola.pr.gov.br>
2024-08-05 22:58:04 +0000

    05/08/24

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const meuNome = "Ana Gabrielly";
 const idade = 17;
 const altura = 1.60;
 const massa = 72;
-const cidade = "Sao Jose dos Pinhais"
+const cidade = "Sao Jose dos Pinhais";
 
 console.log(meuNome);
 console.log(idade);
