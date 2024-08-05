ab27fa245e702d63cd8242d1ad826913056246a8
AnaGab21 <ana.gabriellyde.lima@escola.pr.gov.br>
2024-08-05 22:56:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = "Ana Gabrielly";
+const idade = 17;
+const altura = 1.60;
+const massa = 72;
+const cidade = "Sao Jose dos Pinhais"
+
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
